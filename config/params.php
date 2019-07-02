a3cf4e0770278fef394a309254d6c413b52b00e1
Vlad <excelent91@mail.ru>
2019-07-02 21:35:02 +0300

    init

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'admin_email' => 'admin@mail.com',
+	'shop_name'   => 'ishop store',
+	'pagination'  => 3,
+	'smtp'        => '',
+	'smtp_pass'   => ''
+];
---
+++ vendor/ishop/core/Registry.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ishop;
+
+
+class Registry
+{
+  use TSingletone;
+	protected static $properties = [];
+
+	public function setProperty($name, $value) {
+		self::$properties[$name] = $value;
+	}
+
+	public function getProperty($name) {
+		if(isset(self::$properties[$name])) {
+			return self::$properties[$name];
+		}
+	}
+
+	public function getProperties() {
+		return self::$properties;
+	}
+}
